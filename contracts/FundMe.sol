238f6223bb5c0dad01645c031ee58f8f438b3031
haroon <91939783+haroonalbar@users.noreply.github.com>
2022-04-01 15:13:20 +0530

    making mockV3 for pricefeed

--- contracts/FundMe.sol
+++ contracts/FundMe.sol
@@ -18,10 +18,13 @@ contract FundMe {
     address[] public funders;
     //address of the owner (who deployed the contract)
     address public owner;
+
+    AggregatorV3Interface public priceFeed;
     
     // the first person to deploy the contract is
     // the owner
-    constructor() public {
+    constructor(address _priceFeed) public {
+        priceFeed = AggregatorV3Interface(_priceFeed);
         owner = msg.sender;
     }
     
@@ -37,12 +40,10 @@ contract FundMe {
     
     //function to get the version of the chainlink pricefeed
     function getVersion() public view returns (uint256){
-        AggregatorV3Interface priceFeed = AggregatorV3Interface(0x8A753747A1Fa494EC906cE90E9f37563A8AF630e);
         return priceFeed.version();
     }
     
     function getPrice() public view returns(uint256){
-        AggregatorV3Interface priceFeed = AggregatorV3Interface(0x8A753747A1Fa494EC906cE90E9f37563A8AF630e);
         (,int256 answer,,,) = priceFeed.latestRoundData();
          // ETH/USD rate in 18 digit 
          return uint256(answer * 10000000000);
